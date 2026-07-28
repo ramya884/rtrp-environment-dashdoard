c9a55757df5d9eed42e40f8dbb980d7622513d51
ramya <lakshmisreeramya218@gmail.com>
2026-07-28 22:57:45 +0530

    Update project and main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVR
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-def generate_data(num_records=5000):
+def generate_data(num_records=500):
     np.random.seed(42)
     
     timestamps = pd.date_range(start="2024-01-01", periods=num_records, freq='h')
